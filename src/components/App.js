1ac3f065e2864e29184b1d475423c1da1d228526
sakshi <sakshimatiray96628@gmail.com>
2022-11-28 15:50:55 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,4 @@
-import React from "react";
-import '../styles/App.css';
-import Welcome from './Welcome';
-
-const App = () => {
-  return (
-
-    <h2>Welcome to Newton School.</h2>
-  )
-}
-
+import React from "react";import '../styles/App.css';import Welcome from './Welcome';
+const App = () => {  return (    <Welcome name="" />  )}
 
 export default App;
